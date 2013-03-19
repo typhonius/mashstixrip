d88494c61b91f67a70cb03fe6597537bd16bdeb7
Adam Malone <adam.typhonius@gmail.com>
2013-03-19 16:19:17 +1100

    Added the basics of what I want to do. Going to grab each download page, parse to find the link and then download it (potentially to a chosen location)

--- mashstixrip.pl
+++ mashstixrip.pl
@@ -1,1 +1,14 @@
 #!/usr/bin/perl
+
+use LWP::Simple;
+
+# Ask the user for a download directory?
+
+$path = "http://www.mashstix.com/Downloads/download.php?stixnum=";
+
+$count_404 = 0;
+
+for ($i = 000000; $count_404 <= 5; $i++) {
+  # Get the pages, parse them to find the download link (below) and then grab it.
+  # <a href="https://soundcloud.com/deem-mashup/oppa-soiree-style/download" target="_blank">Download Link (right click)</a>
+}
