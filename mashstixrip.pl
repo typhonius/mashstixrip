c32dc128aad299103c0e2e481a77d2970046daed
Adam Malone <adam.typhonius@gmail.com>
2013-03-20 11:36:52 +1100

    Almost done - the script asks for user input for the download dir and ensures it's writeable. If it doesn't exist it tries to create it - otherwise just loops round. Then it goes and grabs the file. Needs a little bit of niceness to tidy up things like title, file extension etc.

--- mashstixrip.pl
+++ mashstixrip.pl
@@ -3,48 +3,72 @@
 use strict;
 use warnings;
 use LWP::Simple;
-use Data::Dumper;
+use Cwd;
+
+my $cwd = getcwd;
+my $path = "http://www.mashstix.com/Downloads/download.php?stixnum=";
+my $count_404 = 0;
+my $writeable = 0;
 
 sub download_url_parser($) {
   my $raw = shift;
   my $url = '';
-  #print Dumper ($raw);
+  my $title = '';
   foreach (split("\n", $raw)) {
     if ($_ =~ m/<a href=\"(.*)\" target="_blank">Download Link/) {
-  #if ($raw =~ /.*<a href="(\S)" target="_blank">Download Link (right click)<\/a>.*/) {
       $url = $1;
     }
     elsif ($_ =~ m/<title>(.*)<\/title>/) {
-      print "Found $1\n";
+      $title = $1;
+      print "Found $title\n";
     }
   }
-  # <title>MashStix.com Music Mashups - Oki - California Swift (Taylor Swift vs Wolfmother)</title>
-
-  #print Dumper($raw);
-  return $url;
+  return ($url, $title);
 }
 
-# Ask the user for a download directory?
+sub rip_all_the_things() {
+  # currently testing numbers to ensure the pattern matching works
+  for (my $i = 3212; $i <= 3230; $i++) {
+    #print "Processing track id " . $i;
+    $i = sprintf("%6d", $i);
+    $i=~ tr/ /0/;
 
-my $path = "http://www.mashstix.com/Downloads/download.php?stixnum=";
+    # Get the pages, parse them to find the download link (below) and then grab it.
+    my $lwp = LWP::UserAgent->new;
+    my $response = $lwp->get( $path . $i );
+    if (!is_success($response->status_line)) {
+      $count_404++;
+    }
+    else {
+      my ($download_url, $title) = download_url_parser($response->content);
+      getstore($download_url, $directory . $title);
+    }
+  }
+}
 
-my $count_404 = 0;
-# currently testing numbers to ensure the pattern matching works
-for (my $i = 3212; $i <= 3216; $i++) {
-  #print "Processing track id " . $i;
-  $i = sprintf("%6d", $i);
-  $i=~ tr/ /0/;
+# Ask the user for a download directory
+until ($writeable) {
+  print "The current (and default) download directory is $cwd.
+To use this directory type '.' otherwise enter a relative path from
+$cwd or absolute path to proceed.
+Enter the directory to download to: ";
+  # Capture the user input minus any newlines.
+  chomp(our $directory = <>);
 
-  # Get the pages, parse them to find the download link (below) and then grab it.
-  my $lwp = LWP::UserAgent->new;
-  my $response = $lwp->get( $path . $i );
-  if (!is_success($response->status_line)) {
-    $count_404++;
+  # if it's writeable carry on, otherwise try to create a directory,
+  # otherwise just ask again.
+  if (-w $directory && opendir(DL, $directory)) {
+    # The directory is writeable so we can use it.
+    $writeable = 1;
+    rip_all_the_things();
+  }
+  elsif (mkdir $directory) {
+    # Directory isn't writeable, can we create the path by making folders?
+    print "New directory created!\n";
+    $writeable = 1;
+    rip_all_the_things();
   }
   else {
-    #print Dumper ($response->content);
-    #print $path . $i;
-    my $download_url = download_url_parser($response->content);
-  # <a href="https://soundcloud.com/deem-mashup/oppa-soiree-style/download" target="_blank">Download Link (right click)</a>
+    print "Directory not writeable/able to be created. Please specify a new path\n";
   }
 }
